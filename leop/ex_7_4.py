8ba4888b93adde48210cecc9c711c6fe09e1eda2
Maria <leopardilla@protonmail.com>
2021-06-19 16:36:22 +0200

    Fixed the double condition check

--- leop/ex_7_4.py
+++ leop/ex_7_4.py
@@ -1,25 +1,30 @@
-#Дополнения для пиццы: 
+# Дополнения для пиццы:
 
-#напишите цикл, который предлагает пользователю 
-#вводить дополнения для пиццы до тех пор, 
-#пока не будет введено значение 'quit’ . 
-#При вводе каждого дополнения выведите сообщение о том, 
-#что это дополнение включено в заказ.
+# напишите цикл, который предлагает пользователю
+# вводить дополнения для пиццы до тех пор,
+# пока не будет введено значение 'quit’ .
+# При вводе каждого дополнения выведите сообщение о том,
+# что это дополнение включено в заказ.
 
 prompt = "\nTell me please, what would you like to add to your pizza."
 prompt += "\nEnter 'quit' if it's enough. "
 order_is_ready = "\nThank you for your order!"
 
+
 def order_pizza(requested_topping):
+    """The function asks a customer about the toppings for pizza and makes the order"""
     requested_topping = ""
-    while requested_topping != "quit":
+    active = True
+    while active == True:
         requested_topping = input(prompt)
-        message = "We added " + requested_topping + " to your pizza!"
-        if requested_topping != 'quit':
+        if requested_topping != "quit":
+            message = "We added " + requested_topping + " to your pizza!"
             print(message)
         else:
+            active = False
             print(order_is_ready)
             break
+
 
 order_pizza("")
 print("\nWait for your pizza!")
