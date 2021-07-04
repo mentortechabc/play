aa26d673c9a575521eb21e06513e3664babbe84e
eduard <eduardterletskiy@gmail.com>
2021-07-04 20:09:20 +0300

    Revert "check through regular expressions for adding arguments to our format"

--- calendar-admin/delete_interval.py
+++
@@ -1,5 +0,0 @@
-
-def delete_interval(namespace):
-    """удаляем интервал"""
-    print("delete, interval {} to {}".format(
-        namespace.start, namespace.end))
--- calendar-admin/main.py
+++
@@ -1,5 +0,0 @@
-#!/Usr/bin/env python
-import comand_line_parser as par
-
-
-par.check_func()
