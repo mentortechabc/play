4d8a9885ceb981d488d78d746d4fce028c98cbe6
DramaTurG <youremail@yourdomain.com>
2021-07-05 22:00:23 +0200

    delete database

--- calendar-admin/add_interval.py
+++ calendar-admin/add_interval.py
@@ -3,18 +3,17 @@ from datetime import timedelta, datetime
 
 
 def add_interval(namespace):
+    """Добавление интервала в базу данных"""
     namespace_start = datetime.strptime(namespace.start, "%Y-%m-%d:%H:%M")
     namespace_end = datetime.strptime(namespace.end, "%Y-%m-%d:%H:%M")
     interval = namespace_start
     if namespace_start.minute % 15 == 0 and namespace_end.minute % 15 == 0:
         while interval < namespace_end:
             with sq.connect('database.db') as con:
-                cur = con.cursor() #Cursor
+                cur = con.cursor()
 
-                cur.execute("INSERT INTO slot (start) VALUES (?)", [interval])
+                cur.execute("INSERT INTO slot (start_interval) VALUES (?)", [interval])
             print(interval)
             interval += timedelta(minutes=15)
     else:
         print('Введите интервал кратный 15 минутам')
-
-
