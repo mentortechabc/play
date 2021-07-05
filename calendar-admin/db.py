d4fc4a2d943de188e4c6eefb8c41e0e4cd4b51a1
Maria <leopardilla@protonmail.com>
2021-07-05 15:42:51 +0200

    BookongInfo table and slotstable creation functions and tests

--- calendar-admin/db.py
+++ calendar-admin/db.py
@@ -1,6 +1,32 @@
 import sqlite3
 from sqlite3.dbapi2 import Connection
 
+ADMIN_INFO_QUERY = """
+        CREATE TABLE IF NOT EXISTS AdminInfo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL UNIQUE
+    );
+    """
+
+BOOKING_INFO_QUERY = """
+        CREATE TABLE IF NOT EXISTS BookingInfo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        topic TEXT NOT NULL
+    );
+    """
+
+CREATE_SLOTS_QUERY = """
+        CREATE TABLE IF NOT EXISTS Slots (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        start_date TEXT NOT NULL,
+        booking_id INTEGER,
+        FOREIGN KEY(booking_id) REFERENCES BookingInfo(id)
+    );
+    """
+
 
 def create_connection(path_to_db=":memory:") -> Connection:
     """
@@ -10,17 +36,32 @@ def create_connection(path_to_db=":memory:") -> Connection:
     return sqlite3.connect(path_to_db)
 
 
+def create_table(connection: Connection, query: str) -> None:
+    connection.execute(query)
+    connection.commit()
+
+
 def create_admininfo_table(connection: Connection) -> None:
     """
     Creates AdminInfo(id, email, password) table using specified connection.
     """
 
-    connection.execute("""
-        CREATE TABLE IF NOT EXISTS AdminInfo (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL UNIQUE
-    );
-    """)
+    create_table(connection, ADMIN_INFO_QUERY)
 
-    connection.commit()
+
+def create_bookinginfo_table(connection: Connection) -> None:
+    """
+    Creates BookingInfo(id, name, email, topic) table using specified connection.
+    """
+
+    create_table(connection, BOOKING_INFO_QUERY)
+
+
+def create_slots_table(connection: Connection) -> None:
+    """
+    Creates Slots(id, start_date, booking_id) table using specified connection and
+    add foreign key for BookingInfo id.
+    By default guest's info is null or is taken from BookingInfo.
+    """
+
+    create_table(connection, CREATE_SLOTS_QUERY)
--- calendar-admin/tests/test_db.py
+++ calendar-admin/tests/test_db.py
@@ -21,3 +21,49 @@ def test_create_admininfo_table():
     assert result[1] == (2, "edf@example.com", "password")
 
     connection.close()
+
+
+def test_create_bookinginfo_table():
+    connection = db.create_connection()
+
+    db.create_bookinginfo_table(connection)
+
+    connection.execute(
+        "INSERT INTO BookingInfo(name, email, topic) values ('Elon Musk', 'elon_musk@spacex.com', 'contract negotiation')"
+    )
+    connection.execute(
+        "INSERT INTO BookingInfo(name, email, topic) values ('Maye Musk', 'me@mayemusk.com', 'The presentation of the new book')"
+    )
+    cursor = connection.execute("SELECT * FROM BookingInfo")
+    result = cursor.fetchall()
+    assert len(result) == 2
+    assert result[0] == (
+        1, 'Elon Musk', 'elon_musk@spacex.com', 'contract negotiation')
+    assert result[1] == (2, 'Maye Musk', 'me@mayemusk.com',
+                         'The presentation of the new book')
+
+    connection.close()
+
+
+def test_create_slots_table():
+    connection = db.create_connection()
+    db.create_bookinginfo_table(connection)
+    db.create_slots_table(connection)
+
+    connection.execute(
+        "INSERT INTO BookingInfo(name, email, topic) values ('Elon Musk', 'elon_musk@spacex.com', 'contract negotiation')"
+    )
+
+    booking_id = connection.execute("SELECT id FROM BookingInfo").fetchone()[0]
+
+    connection.execute("INSERT INTO slots(start_date, booking_id) values(?, ?)",
+                       ('2021-09-01 12:00:00', booking_id))
+    connection.execute("INSERT INTO slots(start_date, booking_id) values(?, ?)",
+                       ('2021-09-01 12:15:00', booking_id))
+
+    result = connection.execute("SELECT * FROM slots").fetchall()
+    assert len(result) == 2
+    assert result[0] == (1, '2021-09-01 12:00:00', 1)
+    assert result[1] == (2, '2021-09-01 12:15:00', 1)
+
+    connection.close()
