4dc1c71cb6e3af037aa3564f574169f08d1a4681
DramaTurG <youremail@yourdomain.com>
2021-07-05 23:42:24 +0200

    delete_interval

--- calendar-admin/delete_interval.py
+++ calendar-admin/delete_interval.py
@@ -1,5 +1,18 @@
+import sqlite3 as sq
+from datetime import timedelta, datetime
+
 
 def delete_interval(namespace):
     """удаляем интервал"""
-    print("delete, intervsl {} to {}".format(
-        namespace.start, namespace.end))
+    namespace_start = datetime.strptime(namespace.start, "%Y-%m-%d:%H:%M")
+    namespace_end = datetime.strptime(namespace.end, "%Y-%m-%d:%H:%M")
+    interval = namespace_start
+    if namespace_start.minute % 15 == 0 and namespace_end.minute % 15 == 0:
+        while interval < namespace_end:
+            with sq.connect('database.db') as con:
+                cur = con.cursor()
+
+                cur.execute("DELETE FROM slot WHERE start_interval == (?)", [interval])
+            interval += timedelta(minutes=15)
+    else:
+        print('Введите интервал кратный 15 минутам')
