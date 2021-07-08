6893c1f3194efe619aac15e84d3d21d352bc681f
eduard <eduardterletskiy@gmail.com>
2021-07-08 15:08:35 +0300

    Delete get_slots.py

--- calendar-admin/get_slots.py
+++
@@ -1,9 +0,0 @@
-
-def get_slots(namespace):
-    """получаем список слотов"""
-    if namespace.week:
-        print("week slots: {}".format(namespace.week))
-    if namespace.day:
-        print("day slots: {}".format(namespace.day))
-    if namespace.filter:
-        print("filter: {}".format(namespace.filter))
