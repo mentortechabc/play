b7d1bda9be45c045832d6ee766f7cc667d6e1eb3
mentortechabc <mentortechabc@users.noreply.github.com>
2021-07-08 21:34:37 +0300

    add doc string for flask server

--- homework-python/http/flask_server.py
+++ homework-python/http/flask_server.py
@@ -1,3 +1,21 @@
+"""
+how to run
+1) install flask (maybe using virtualenvs)
+2) change directory accordingly
+3) export FLASK_APP=flask_server.py
+4) python -m flask run
+
+You must see something like
+
+     * Serving Flask app 'flask_server.py' (lazy loading)
+     * Environment: production
+       WARNING: This is a development server. Do not use it in a production deployment.
+       Use a production WSGI server instead.
+     * Debug mode: off
+     * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
