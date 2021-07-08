df066408b7937384a1671e888e9361d73a660f7c
eduard <you@example.com>
2021-07-08 15:12:06 +0300

    added correct regex for interval and day

--- calendar-admin/comand_line_parser.py
+++ calendar-admin/comand_line_parser.py
@@ -24,7 +24,7 @@ def createParser():
     delete_day.add_argument('date', help="date YYYY-MM-DD")
 
     get_slots = subparsers.add_parser('get_slots')
-    get_slots.add_argument('--week', '-w', help="#######")
+    get_slots.add_argument('--week', '-w', help="7 days from date YYYY-MM-DD")
     get_slots.add_argument('--day', '-d', help="day YYYY-MM-DD")
     get_slots.add_argument(
         '--filter', '-f', help="booking or free", choices=['booking', 'free'])
@@ -43,7 +43,7 @@ def check_func():
     elif params.command == "delete_interval":
         check_format_add_interval(params, di.delete_interval)
     elif params.command == "delete_day":
-        chech_format_delete_day(params)
+        check_format_delete_day(params)
     elif params.command == "get_slots":
         check_format_get_slots(params)
     else:
@@ -57,7 +57,8 @@ def check_format_add_interval(params, func):
     else:
         print("wrong format add_interval")
 
-def chech_format_delete_day(params):
+
+def check_format_delete_day(params):
     """запуск функции delete_day, если аргументы соответствуют условию"""
     if regular_day(params.date) is True:
         dd.delete_day(params)
@@ -67,24 +68,16 @@ def chech_format_delete_day(params):
 
 def check_format_get_slots(params):
     """запуск функции get_slots, если аргументы соответствуют условию"""
-    if (check_week(params) is False) or (check_day(params) is False) or (check_filter(params) is False):
+    if (check_day_week(params.week) is False) or (check_day_week(params.day) is False) or (check_filter(params) is False):
         print("wrong format get_slots")
     else:
         gs.get_slots(params)
 
 
-def check_week(params):
-    """проверяет что week не равен None"""
-    if params.week is not None:
-        return regular_week(params.week)
-    else:
-        return True
-
-
-def check_day(params):
+def check_day_week(params):
     """проверка что day не равен None"""
-    if params.day is not None:
-        return regular_day(params.day)
+    if params is not None:
+        return regular_day(params)
     else:
         return True
 
@@ -107,7 +100,8 @@ def regular_filter(x):
 
 def regular_start_end(x):
     """решулярка для проверки формата start и end[YYYY-MM-DD:HH:MM]"""
-    pattern = r'[0-9]{4}[-][0-9]{2}[-][0-9]{2}[:][0-9]{2}[:][0-9]{2}$'
+    # pattern = r'[0-9]{4}[-][0-9]{2}[-][0-9]{2}[:][0-9]{2}[:][0-9]{2}$'
+    pattern = r'^([0-9]{4}[-]?((0[13-9]|1[012])[-]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-]?31|02[-]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048])00)[-]?02[-]?29)[:](0[0-1]|1[0-9]|2[0-3])[:]([0-5]{1}[0-9]{1})$'
     if re.match(pattern, x):
         return True
     else:
@@ -116,7 +110,8 @@ def regular_start_end(x):
 
 def regular_day(x):
     """регулярка для проверки формата date[YYYY-MM-DD]"""
-    pattern = r'[0-9]{4}[-][0-9]{2}[-][0-9]{2}$'
+    # pattern = r'[0-9]{4}[-][0-9]{2}[-][0-9]{2}$'
+    pattern = r'^([0-9]{4}[-]?((0[13-9]|1[012])[-]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-]?31|02[-]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048])00)[-]?02[-]?29)$'
     if re.match(pattern, x):
         return True
     else:
