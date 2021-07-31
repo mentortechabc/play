ffeb442a22a1e9ab6d69f3832e478c907fe7c210
tmnt <sergey1kabanov@gmail.com>
2021-07-31 17:53:18 +0200

    refactor(sanity_test) Improve test_sanity.py
    - Make the test cross-platform / cross-python.
    - Remove some code-duplication.
    - Add expectations.

--- calendar-admin/tests/test_sanity.py
+++ calendar-admin/tests/test_sanity.py
@@ -1,9 +1,25 @@
 import subprocess
+import sys
+import os
+
+
+PYTHON_BINARY = sys.executable
+
+
+def run_with_args(args):
+    return subprocess.check_output(
+        "{} main.py -p test_main_db.sqlite {}".format(PYTHON_BINARY, args),
+        shell=True)
+
 
 def test_sanity():
-    subprocess.check_call("del test_main_db.sqlite", shell=True)
-    subprocess.check_call("python main.py -p test_main_db.sqlite add_interval 2021-05-03T10:00 2021-05-10T11:15", shell=True)
-    subprocess.check_output("python main.py -p test_main_db.sqlite get_slots -d 2021-05-03", shell=True)
-    subprocess.check_output("python main.py -p test_main_db.sqlite get_slots -w 2021-05-03", shell=True)
-    subprocess.check_call("python main.py -p test_main_db.sqlite delete_day 2021-05-03", shell=True)
-    subprocess.check_call("python main.py -p test_main_db.sqlite delete_interval 2021-05-03T10:00 2021-05-08T11:15", shell=True)
+    try:
+        os.remove('test_main_db.sqlite')
+    except FileNotFoundError:
+        pass
+
+    assert run_with_args("add_interval 2021-05-03T10:00 2021-05-10T11:15") == b""
+    assert run_with_args("get_slots -d 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-04 00:15:00\r\n"
+    assert run_with_args("get_slots -w 2021-05-03") == b"2021-05-03 10:00:00 - 2021-05-10 00:15:00\r\n"
+    assert run_with_args("delete_day 2021-05-03") == b""
+    assert run_with_args("delete_interval 2021-05-03T10:00 2021-05-08T11:15") == b""
